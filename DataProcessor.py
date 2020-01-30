3fe2d7a01d0844ffd9c72baa00a270972370121d
igcomsrl <59048310+igcomsrl@users.noreply.github.com>
2020-01-30 14:31:29 +0100

    Update DataProcessor.py

--- DataProcessor.py
+++ DataProcessor.py
@@ -8,6 +8,8 @@ class DataProcessor:
         self.defaults = defaults
 
     def _removeColumns(self, dataframes: List[pd.DataFrame]) -> List[pd.DataFrame]:
+        """ Funzione che rimuove le colonne aggiuntive ritornate dalla query al database influx. """
+
         toRet = []
 
         for dataframe, metric in zip(dataframes, self.metrics):
@@ -18,6 +20,8 @@ class DataProcessor:
         return toRet
 
     def _modifyTimestamp(self, dataframes: List[pd.DataFrame]) -> List[pd.DataFrame]:
+        """ Funzione che modifica il timestamp di ogni misurazione in slot di tempo per facilitare i controlli seguenti"""
+
         toRet = []
 
         for dataframe in dataframes:
@@ -33,6 +37,8 @@ class DataProcessor:
         return toRet
 
     def _collapseData(self, dataframes: List[pd.DataFrame]) -> List[pd.DataFrame]:
+        """ Funzione che raggruppa i dati per processInstanceId e per slot di tempo """
+
         toRet = []
 
         for dataframe in dataframes:
@@ -44,6 +50,8 @@ class DataProcessor:
         return toRet
 
     def _joinData(self, dataframes: List[pd.DataFrame]) -> pd.DataFrame:
+        """ Funzione che esegue il join tra le misurazioni delle diverse metriche basandosi sullo slot di tempo e sul processInstanceId """
+
         df = dataframes[0]
 
         for i in range(1, len(dataframes)):
@@ -52,19 +60,23 @@ class DataProcessor:
         return df
 
     def _fillNaNs(self, df: pd.DataFrame) -> pd.DataFrame:
+        """ Funzione che sovrascrive i valori non presenti all'interno del database """
+
         sorted = df.sort_values('time')
         grouped = sorted.groupby('processInstanceId')
-        forwardFilled = grouped.ffill()
+        forwardFilled = grouped.ffill() # Provo a sovrascrivere i valori non presenti con l'ultima misurazione della corrispettiva metrica
         forwardFilled['processInstanceId'] = sorted['processInstanceId']
-        beforeFilled = forwardFilled.groupby(['processInstanceId']).bfill()
+        beforeFilled = forwardFilled.groupby(['processInstanceId']).bfill() # provo a sovrascrivere i valori non presenti con la prossima misurazione della corrispettiva metrica
         beforeFilled['processInstanceId'] = sorted['processInstanceId']
 
-        for metric, default in zip(self.metrics, self.defaults):
+        for metric, default in zip(self.metrics, self.defaults): # se in entrambi i precedenti casi non sono riuscito a sostituire i dati mancanti allora sovrascrivo con valori di default
             beforeFilled['{}'.format(metric)].fillna(default, inplace=True)
 
         return beforeFilled
 
     def applyPipeline(self, dataframes: List[pd.DataFrame]) -> pd.DataFrame:
+        """ Applica tutte le funzioni precedentemente definite in cascata. """
+        
         return self._fillNaNs(
             self._joinData(
                 self._collapseData(
